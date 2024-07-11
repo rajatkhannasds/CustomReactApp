36d621bb3bc78c4a5dd79b120d418d2a059c343f
Rajat Khanna <rajatkhannasds@gmail.com>
2024-07-11 17:08:32 +0530

    Custom react element created

---
+++ customReact.js
@@ -0,0 +1,28 @@
+const mainContainer = document.querySelector("#root");
+
+function customRender(reactElement, container)  {
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    // domElement.setAttribute('href', reactElement.props.href);
+    // domElement.setAttribute('target', reactElement.props.target);
+    for (const prop in reactElement.props) {
+        if(prop === 'children') 
+            continue;
+        
+        domElement.setAttribute(prop, reactElement.props[prop]);
+        
+    }
+    container.appendChild(domElement);
+}
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://www.google.com",
+        target:"_blank"
+    },
+    children: "Click me to visit google. "
+}
+
+customRender(reactElement, mainContainer);
